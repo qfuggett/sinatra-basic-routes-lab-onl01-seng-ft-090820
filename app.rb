86780a6391e03be3476cbd1472e3a97b4de4fc70
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-14 22:23:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Sabrina"
+  end
+  
+  get '/hometown' do
+    "My hometown is Salem, WI"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Halloween Town"
+  end
+  
 end
